486af3075b6950fcc1be84fff122d41dc8043df0
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2022-06-22 20:21:29 +0530

    update demo

--- demo-ts/index.ts
+++ demo-ts/index.ts
@@ -1,25 +1,37 @@
-import CSVFileValidator, { ParsedResults, ValidatorConfig } from '../src/csv-file-validator'
+import CSVFileValidator, {
+	ParsedResults,
+	RowError,
+	ValidatorConfig,
+} from "../src/csv-file-validator";
 
-const requiredError = (headerName: string, rowNumber: number, columnNumber: number) => {
-	return `<div class="red">${headerName} is required in the <strong>${rowNumber} row</strong> / <strong>${columnNumber} column</strong></div>`
-}
+const requiredError = (
+	headerName: string,
+	rowNumber: number,
+	columnNumber: number
+) => {
+	return `<div class="red">${headerName} is required in the <strong>${rowNumber} row</strong> / <strong>${columnNumber} column</strong></div>`;
+};
 
-const validateError = (headerName: string, rowNumber: number, columnNumber: number) => {
-	return `<div class="red">${headerName} is not valid in the <strong>${rowNumber} row</strong> / <strong>${columnNumber} column</strong></div>`
-}
+const validateError = (
+	headerName: string,
+	rowNumber: number,
+	columnNumber: number
+) => {
+	return `<div class="red">${headerName} is not valid in the <strong>${rowNumber} row</strong> / <strong>${columnNumber} column</strong></div>`;
+};
 
 const uniqueError = (headerName: string, rowNumber: number) => {
-	return `<div class="red">${headerName} is not unique at the <strong>${rowNumber} row</strong></div>`
-}
+	return `<div class="red">${headerName} is not unique at the <strong>${rowNumber} row</strong></div>`;
+};
 
 const isEmailValid = function (email: string) {
-	const reqExp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
-	return reqExp.test(email)
-}
+	const reqExp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+	return reqExp.test(email);
+};
 
 const isPasswordValid = function (password: string) {
-	return password.length >= 4
-}
+	return password.length >= 4;
+};
 
 interface CSVRow {
 	firstName: string;
@@ -31,21 +43,57 @@ interface CSVRow {
 
 const CSVConfig: ValidatorConfig = {
 	headers: [
-		{ name: 'First Name', inputName: 'firstName', required: true, requiredError },
-		{ name: 'Last Name', inputName: 'lastName', required: true, requiredError, optional: true },
-		{ name: 'Email', inputName: 'email', required: true, requiredError, unique: true, uniqueError, validate: isEmailValid, validateError },
-		{ name: 'Password', inputName: 'password', required: true, requiredError, validate: isPasswordValid, validateError },
-		{ name: 'Roles', inputName: 'roles', required: true, requiredError, isArray: true }
-	]
-}
+		{
+			name: "First Name",
+			inputName: "firstName",
+			required: true,
+			requiredError,
+		},
+		{
+			name: "Last Name",
+			inputName: "lastName",
+			required: true,
+			requiredError,
+			optional: true,
+		},
+		{
+			name: "Email",
+			inputName: "email",
+			required: true,
+			requiredError,
+			unique: true,
+			uniqueError,
+			validate: isEmailValid,
+			validateError,
+		},
+		{
+			name: "Password",
+			inputName: "password",
+			required: true,
+			requiredError,
+			validate: isPasswordValid,
+			validateError,
+		},
+		{
+			name: "Roles",
+			inputName: "roles",
+			required: true,
+			requiredError,
+			isArray: true,
+		},
+	],
+};
 
-document.getElementById('file').onchange = function (event: any) {
-	CSVFileValidator<CSVRow>(event.target.files[0], CSVConfig)
-		.then((csvData: ParsedResults) => {
-			csvData.inValidMessages.forEach((message: string) => {
-				document.getElementById('invalidMessages').insertAdjacentHTML('beforeend', message)
-			})
-			console.log(csvData.inValidMessages)
-			console.log(csvData.data)
-		})
-}
+document.getElementById("file")!.onchange = function (event: any) {
+	CSVFileValidator<CSVRow>(event.target.files[0], CSVConfig).then(
+		(csvData: ParsedResults) => {
+			csvData.inValidMessages.forEach((message: RowError) => {
+				document
+					.getElementById("invalidMessages")
+					?.insertAdjacentHTML("beforeend", message.message);
+			});
+			console.log(csvData.inValidMessages);
+			console.log(csvData.data);
+		}
+	);
+};
--- demo/index.js
+++ demo/index.js
@@ -35,7 +35,7 @@ document.getElementById('file').onchange = function (event) {
 	CSVFileValidator(event.target.files[0], CSVConfig)
 		.then(csvData => {
 			csvData.inValidMessages.forEach(message => {
-				document.getElementById('invalidMessages').insertAdjacentHTML('beforeend', message)
+				document.getElementById('invalidMessages').insertAdjacentHTML('beforeend', message.message)
 			})
 			console.log(csvData.inValidMessages)
 			console.log(csvData.data)
